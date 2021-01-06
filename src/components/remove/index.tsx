f2c3cda323e071dd124873182dfecc2fd84ea160
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-06 19:56:34 +0100

    feat: Remove icon

---
+++ src/components/remove/index.tsx
@@ -0,0 +1,8 @@
+import remove from '../../assets/remove.svg'
+import styled from 'styled-components'
+
+const RemoveImage = styled.img``
+
+export const Remove = () => (
+  <RemoveImage src={remove} alt="Remove image from list" />
+)
