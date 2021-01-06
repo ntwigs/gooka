6c6f3702a32a9cd44b1d61a1e8c95d30dc95cb87
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-06 19:43:48 +0100

    feat: Add colors to default theme

---
+++ src/theme/index.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  colors: {
+    black: '#232323',
+    blue: '#008CFF',
+    green: '#08C988',
+    grey: '#4F4F4F',
+    white: '#FFFFFF',
+    light: '#BDBDBD',
+    red: '#F76345',
+  },
+}
