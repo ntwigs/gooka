876cd86ec9e8a68d19af58ca426736c596442b96
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-07 22:22:32 +0100

    feat: File and style section from state

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -10,19 +10,69 @@ import { Box, Sizes } from '../../components/box'
 import { ResultHeaderSection } from '../../components/result-header-section'
 import { UnnecessaryClassname } from '../../components/unnecessary-classname'
 import { StyleDropzone } from '../../components/style-dropzone'
-const { ipcRenderer } = window.require('electron')
+import { FileDropzone } from '../../components/file-dropzone'
+import { useSelector } from 'react-redux'
+import { getFiles } from '../../redux/selectors/files'
+import { getStyle } from '../../redux/selectors/style'
+
+const StyleSection = () => {
+  const { style } = useSelector(getStyle)
+
+  const hasNameAndPath = style.name && style.path
+
+  return (
+    <Box marginBottom={Sizes.xl}>
+      <SectionHeader
+        title="Drop style to check"
+        subtitle="Drag style-files from the vscode explorer and drop them here."
+      />
+      {!hasNameAndPath && <StyleDropzone />}
+      {hasNameAndPath && (
+        <File
+          name={style.name}
+          path={style.path}
+          id={style.id}
+          fileType="style"
+        />
+      )}
+    </Box>
+  )
+}
+
+const FileRenderSection = () => {
+  const { files } = useSelector(getFiles)
+
+  const Files = () => (
+    <>
+      {files.map(({ id, name, path }: any) => (
+        <File key={id} id={id} name={name} path={path} fileType="file" />
+      ))}
+    </>
+  )
+
+  return (
+    <Box>
+      <SectionHeader
+        title="Drop files to compare"
+        subtitle="Drag files from the vscode explorer and drop them here."
+      />
+      <Files />
+      <FileDropzone />
+    </Box>
+  )
+}
+
+// React.useEffect(() => {
+//   console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+//   ipcRenderer.on('asynchronous-reply', (event: any, arg: any) => {
+//     console.log(arg) // prints "pong"
+//   })
+
+//   ipcRenderer.send('asynchronous-message', 'ping')
+// }, [])
 
 export const Start = () => {
-  React.useEffect(() => {
-    console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
-
-    ipcRenderer.on('asynchronous-reply', (event: any, arg: any) => {
-      console.log(arg) // prints "pong"
-    })
-
-    ipcRenderer.send('asynchronous-message', 'ping')
-  }, [])
-
   return (
     <Section>
       <HeaderSection>
@@ -30,21 +80,8 @@ export const Start = () => {
       </HeaderSection>
       <Box marginLeft={Sizes.xl} marginRight={Sizes.xl}>
         <FileSection>
-          <Box marginBottom={Sizes.xl}>
-            <SectionHeader
-              title="Drop style to check"
-              subtitle="Drag style-files from the vscode explorer and drop them here."
-            />
-            <StyleDropzone />
-            <File name="card.scss" path="components/shared/card" />
-          </Box>
-          <SectionHeader
-            title="Drop files to compare"
-            subtitle="Drag files from the vscode explorer and drop them here."
-          />
-          <File name="cardTag.tsx" path="components/shared/card" />
-          <File name="cardTitle.tsx" path="components/shared/card" />
-          <File name="cardImage.tsx" path="components/shared/card" />
+          <StyleSection />
+          <FileRenderSection />
         </FileSection>
       </Box>
       <ResultHeaderSection>
