cd659361953d22f2b741968edd5ec4eb5d321aeb
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-11 21:00:12 +0100

    feat: Add style actions

--- src/redux/actions/files.ts
+++ src/redux/actions/files.ts
@@ -1,8 +1,13 @@
-import { RESET, SET_FILES, REMOVE_FILE } from '../types/files'
+import {
+  RESET,
+  SET_FILES,
+  REMOVE_FILE,
+  REMOVE_STYLE,
+  SET_STYLES,
+  FileProps,
+} from '../types/files'
 
-export const setFiles = (
-  files: { name: string; path: string; id: string }[],
-) => ({
+export const setFiles = (files: FileProps[]) => ({
   type: SET_FILES,
   payload: {
     content: files,
@@ -15,5 +20,21 @@ export const removeFile = (id: string) => ({
 })
 
 export const resetFiles = () => ({
+  type: RESET,
+})
+
+export const setStyles = (styles: FileProps[]) => ({
+  type: SET_STYLES,
+  payload: {
+    content: styles,
+  },
+})
+
+export const removeStyle = (id: string) => ({
+  type: REMOVE_STYLE,
+  payload: { content: id },
+})
+
+export const resetStyle = () => ({
   type: RESET,
 })
