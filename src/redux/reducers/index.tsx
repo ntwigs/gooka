f5110b64f0c83284aeca1404fcae9c86dd81911f
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-11 21:00:34 +0100

    fix: Remove unused reducers

--- src/redux/reducers/index.tsx
+++ src/redux/reducers/index.tsx
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'
 import { files } from './files'
-import { style } from './style'
 
-export const rootReducer = combineReducers({ files, style })
+export const rootReducer = combineReducers({ files })
