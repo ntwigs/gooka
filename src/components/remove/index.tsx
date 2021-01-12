b9cdc0c3f826043a9164f8946059dfb915aae5e9
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-12 19:33:34 +0100

    feat: Animate remove button

--- src/components/remove/index.tsx
+++ src/components/remove/index.tsx
@@ -2,8 +2,22 @@ import * as React from 'react'
 import remove from '../../assets/remove.svg'
 import styled from 'styled-components'
 
-const RemoveImage = styled.img``
+type RemoveImageProps = {
+  isRemoved?: boolean
+}
 
-export const Remove = () => (
-  <RemoveImage src={remove} alt="Remove image from list" />
+const RemoveImage = styled.img<RemoveImageProps>`
+  cursor: pointer;
+  pointer-events: all;
+  transition: all 250ms;
+  transform: ${({ isRemoved }) =>
+    isRemoved ? 'rotate(45deg)' : 'rotate(0deg)'};
+`
+
+export const Remove = ({ isRemoved }: RemoveImageProps) => (
+  <RemoveImage
+    isRemoved={isRemoved}
+    src={remove}
+    alt="Remove classname from list"
+  />
 )
