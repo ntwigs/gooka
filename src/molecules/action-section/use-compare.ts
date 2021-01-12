6de472b3bfb37db54cf77974925a8ae27a6b21c5
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-12 19:34:03 +0100

    fix: Update useCompare

--- src/molecules/action-section/use-compare.ts
+++ src/molecules/action-section/use-compare.ts
@@ -1,17 +1,18 @@
 import { useSelector } from 'react-redux'
 import { getFiles, getStyles } from 'src/redux/selectors/files'
+import { hasElements } from 'src/utils/has-elements'
 import { ipcRenderer } from 'src/utils/ipcRender'
 
 export const useCompare = () => {
   const files = useSelector(getFiles)
-  const style = useSelector(getStyles)
+  const styles = useSelector(getStyles)
 
   return () => {
-    const hasFiles = Array.isArray(files) && files.length > 0
-    const hasStyle = style.name && style.path
+    const hasFiles = hasElements(files)
+    const hasStyles = hasElements(styles)
 
-    if (hasFiles && hasStyle) {
-      ipcRenderer.send('analyze-classnames', { files, style })
+    if (hasFiles && hasStyles) {
+      ipcRenderer.send('analyze-classnames', { files, styles })
     }
   }
 }
