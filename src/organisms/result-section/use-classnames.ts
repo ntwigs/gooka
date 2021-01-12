0991ef016d45f5405525e061b0fca0ad2ac5936a
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-12 19:31:41 +0100

    fix: Update useClassnames

--- src/organisms/result-section/use-classnames.ts
+++ src/organisms/result-section/use-classnames.ts
@@ -1,15 +1,33 @@
 import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { v4 as uuid } from 'uuid'
+import { setClassnames } from 'src/redux/actions/files'
+import { getFiles, getStyles } from 'src/redux/selectors/files'
+import { ClassnameProps } from 'src/redux/types/files'
 import { hasElements } from 'src/utils/has-elements'
 import { ipcRenderer } from 'src/utils/ipcRender'
 
 export const useClassnames = () => {
-  const [classnames, setClassnames] = useState<string[]>([])
+  const dispatch = useDispatch()
+  const files = useSelector(getFiles)
+  const styles = useSelector(getStyles)
+
+  const hasFiles = hasElements(files)
+  const hasStyles = hasElements(styles)
 
   useEffect(() => {
-    ipcRenderer.on('analyze-classnames', (_: unknown, arg: string[]) => {
-      if (hasElements(arg)) setClassnames(arg)
+    dispatch(setClassnames([]))
+  }, [hasFiles, hasStyles])
+
+  useEffect(() => {
+    ipcRenderer.on('analyze-classnames', (_: unknown, classnames: string[]) => {
+      if (hasElements(classnames)) {
+        const classnamesWithId = classnames.map((classname) => ({
+          name: classname,
+          id: uuid(),
+        }))
+        dispatch(setClassnames(classnamesWithId))
+      }
     })
   }, [])
-
-  return classnames
 }
