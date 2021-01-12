9faa1f16683549d90dd5d47264fd45a12d99c9b9
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-12 19:31:08 +0100

    feat: Classname props

--- src/redux/types/files.ts
+++ src/redux/types/files.ts
@@ -3,9 +3,15 @@ export const REMOVE_FILE = 'REMOVE_FILE'
 export const SET_STYLES = 'SET_STYLE'
 export const REMOVE_STYLE = 'REMOVE_STYLE'
 export const RESET = 'RESET'
+export const SET_CLASSNAMES = 'SET_CLASSNAMES'
 
 export type FileProps = {
   name: string
   path: string
+  id: string
+}
+
+export type ClassnameProps = {
+  name: string
   id: string
 }
