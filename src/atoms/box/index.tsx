b6189f455e185b0d3f82c556e1d45e54994d778a
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-12 19:32:39 +0100

    chore: Move box to atoms

---
+++ src/atoms/box/index.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export enum Sizes {
+  s,
+  m,
+  l,
+  xl,
+}
+
+type BoxProps = {
+  marginTop?: Sizes
+  marginRight?: Sizes
+  marginBottom?: Sizes
+  marginLeft?: Sizes
+  row?: boolean
+}
+
+type SizeProps = { [props: string]: number }
+
+const sizes: SizeProps = {
+  [Sizes.s]: 10,
+  [Sizes.m]: 15,
+  [Sizes.l]: 25,
+  [Sizes.xl]: 50,
+}
+
+export const Box = styled.div<BoxProps>`
+  display: flex;
+  flex-direction: ${(props) => (props.row ? 'row' : 'column')};
+  min-width: 0;
+  margin-top: ${({ marginTop }) => (marginTop ? sizes[marginTop] : 0)}px;
+  margin-right: ${({ marginRight }) =>
+    marginRight ? sizes[marginRight] : 0}px;
+  margin-bottom: ${({ marginBottom }) =>
+    marginBottom ? sizes[marginBottom] : 0}px;
+  margin-left: ${({ marginLeft }) => (marginLeft ? sizes[marginLeft] : 0)}px;
+`
