1d11092d437c5cb9196281b3ae0c962504078355
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-12 19:30:13 +0100

    feat: Classnames

---
+++ electron/models/classname.ts
@@ -0,0 +1,7 @@
+import { getUnused } from '../resolvers/get-unused'
+import { getUniqueClassnames } from '../resolvers/get-unique-classnames'
+
+export const classnames = {
+  getUnused,
+  getUniqueClassnames,
+}
