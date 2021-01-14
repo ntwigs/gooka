86b224f467530db7627d8daafd0464661a701e9c
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-14 19:52:00 +0100

    feat: Logo placeholder

---
+++ src/components/atoms/logo/index.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Logo = styled.div`
+  width: 60px;
+  height: 60px;
+  background: ${({ theme }) => theme.colors.green};
+  border-radius: 30px;
+`
