8a3d177c089917900218c7797eb3ac692ca66a54
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-14 19:54:05 +0100

    feat: Use drawer for file section

--- src/components/organisms/file-section/index.tsx
+++ src/components/organisms/file-section/index.tsx
@@ -1,39 +1,43 @@
 import React from 'react'
 import { Box, Sizes } from '../../atoms/box'
 import { DropSection } from '../../molecules/drop-section'
-import { Header } from '../../molecules/header'
 import { setFiles, setStyles } from '../../../redux/actions/files'
 import { getStyles, getFiles } from '../../../redux/selectors/files'
 import { ActionSection } from '../action-section'
 import { useInfo } from './use-info'
+import { Drawer } from '../../atoms/drawer'
+import { LogoHeader } from '../../molecules/logo-header'
+import { Scroll } from '../../atoms/scroll'
 
 export const FileSection = () => {
   const onFileDrop = useInfo({ setAction: setFiles })
   const onStyleDrop = useInfo({ setAction: setStyles })
 
   return (
-    <Box marginLeft={Sizes.xl} marginRight={Sizes.xl}>
-      <Header title="Gooka" subtitle="Remove unnecessary css classes." />
-      <Box marginBottom={Sizes.s}>
+    <Drawer>
+      <LogoHeader />
+      <Scroll>
+        <Box marginBottom={Sizes.s}>
+          <DropSection
+            title="Files"
+            subtitle="Drop files to compare with here."
+            dropTitle="Drop style here"
+            onDrop={onStyleDrop}
+            selector={getStyles}
+            fileType="style"
+            maxFiles={1}
+          />
+        </Box>
         <DropSection
-          title="Drop style to check"
-          subtitle="Drag style-files from the vscode explorer and drop them here."
-          dropTitle="Drop style here"
-          onDrop={onStyleDrop}
-          selector={getStyles}
-          fileType="style"
-          maxFiles={1}
+          title="Styles"
+          subtitle="Drop styles to compare with here."
+          dropTitle="Drop files here"
+          onDrop={onFileDrop}
+          selector={getFiles}
+          fileType="file"
         />
-      </Box>
-      <DropSection
-        title="Drop files to compare"
-        subtitle="Drag files from the vscode explorer and drop them here."
-        dropTitle="Drop files here"
-        onDrop={onFileDrop}
-        selector={getFiles}
-        fileType="file"
-      />
+      </Scroll>
       <ActionSection />
-    </Box>
+    </Drawer>
   )
 }
