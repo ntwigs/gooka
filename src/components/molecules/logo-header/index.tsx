d40fa06b5e57058545b01cc8613691c7c4608116
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-14 19:53:27 +0100

    feat: Logo header

---
+++ src/components/molecules/logo-header/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import { Logo } from '../../atoms/logo'
+
+const LogoHeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 156px;
+  padding: 0 40px;
+`
+
+const Name = styled.p`
+  color: ${({ theme }) => theme.colors.green};
+  font-weight: bold;
+  font-size: 34px;
+  margin-left: 10px;
+`
+
+export const LogoHeader = () => {
+  return (
+    <LogoHeaderContainer>
+      <Logo />
+      <Name>Gooka</Name>
+    </LogoHeaderContainer>
+  )
+}
