def79f13b5c79607fda1a676cc104f78f234c12b
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-15 21:17:26 +0100

    refactor: Move file actions to classname

--- main/actions/file.ts
+++ main/actions/file.ts
@@ -1,11 +1,7 @@
-import { getClassnames } from '../resolvers/file/get-classnames'
 import { getPath } from '../resolvers/file/get-path'
 import { getContent } from '../resolvers/file/get-content'
-import { getCleanClassnames } from '../resolvers/file/get-clean-classnames'
 
 export const file = {
   getContent,
   getPath,
-  getClassnames,
-  getCleanClassnames,
 }
