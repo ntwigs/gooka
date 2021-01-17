0bee7d4fd8cb66d5ecb26b662335578695df97b0
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-17 19:55:44 +0100

    feat: New remove icon with animation

--- src/components/atoms/trash/index.tsx
+++ src/components/atoms/trash/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
-import trash from '../../../assets/delete.svg'
+import remove from '../../../assets/remove.svg'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const TrashImage = styled.img`
   cursor: pointer;
@@ -9,8 +10,30 @@ const TrashImage = styled.img`
 
 type TrashProps = {
   onClick: () => void
+  isActive?: boolean
 }
 
-export const Trash = ({ onClick }: TrashProps) => (
-  <TrashImage onClick={onClick} src={trash} alt="Remove file from list" />
+const TrashContainer = styled(motion.div)`
+  width: 25px;
+  height: 25px;
+  border-radius: 5px;
+  background: ${({ theme }) => theme.colors.red};
+`
+
+const variants = {
+  active: { opacity: 1, x: 0 },
+  inactive: { opacity: 0, x: -10 },
+}
+
+export const Trash = ({ onClick, isActive }: TrashProps) => (
+  <TrashContainer
+    initial={false}
+    transition={{ duration: 0.1, type: 'spring', mass: 0.1 }}
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
+    animate={isActive ? 'active' : 'inactive'}
+    variants={variants}
+  >
+    <TrashImage onClick={onClick} src={remove} alt="Remove file from list" />
+  </TrashContainer>
 )
