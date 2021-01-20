f1ede0348b255bf5381256a382bcd796745f76a6
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-20 19:11:40 +0100

    feat: Animate logo

--- src/components/molecules/logo-header/index.tsx
+++ src/components/molecules/logo-header/index.tsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 import * as React from 'react'
 import styled from 'styled-components'
 import { Logo } from '../../atoms/logo'
+import { variants } from '../../../animations/animation'
 
 const LogoHeaderContainer = styled.div`
   display: flex;
@@ -12,31 +13,18 @@ const LogoHeaderContainer = styled.div`
 
 const Name = styled(motion.p)`
   color: ${({ theme }) => theme.colors.green};
-  font-weight: bold;
+  font-weight: 900;
   font-size: 34px;
   margin-left: 10px;
 `
 
-export const LogoHeader = React.forwardRef((_, ref) => {
+export const LogoHeader = () => {
   return (
     <LogoHeaderContainer>
-      <Logo
-        ref={ref as any}
-        animate={{ y: 0, opacity: 1 }}
-        initial={{ y: 20, opacity: 0 }}
-        transition={{
-          type: 'spring',
-          delay: 0.25,
-          mass: 2,
-        }}
-      />
-      <Name
-        animate={{ y: 0, opacity: 1 }}
-        initial={{ y: 20, opacity: 0 }}
-        transition={{ type: 'spring', delay: 0.5, mass: 2 }}
-      >
+      <Logo variants={variants(1)} animate={'animate'} initial={'initial'} />
+      <Name variants={variants(1.1)} animate={'animate'} initial={'initial'}>
         Gooka
       </Name>
     </LogoHeaderContainer>
   )
-})
+}
