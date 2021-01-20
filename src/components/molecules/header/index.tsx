d72e89ae2dfc0adceaeb58ee5b6f2355be510b81
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-20 19:11:34 +0100

    feat: Animate section header

--- src/components/molecules/header/index.tsx
+++ src/components/molecules/header/index.tsx
@@ -1,18 +1,33 @@
+import { motion } from 'framer-motion'
 import * as React from 'react'
+import { variants } from '../../../animations/animation'
 import { Box, Sizes } from '../../atoms/box'
 
 type HeaderProps = {
   title: string
   subtitle: string
+  delay?: number
 }
 
-export const Header = ({ title, subtitle }: HeaderProps) => {
+export const Header = ({ title, subtitle, delay = 0 }: HeaderProps) => {
   return (
     <Box marginBottom={Sizes.m} marginTop={Sizes.m}>
-      <Box marginBottom={Sizes.s}>
-        <h1>{title}</h1>
-      </Box>
-      <h3>{subtitle}</h3>
+      <motion.div
+        variants={variants(delay)}
+        animate="animate"
+        initial="initial"
+      >
+        <Box marginBottom={Sizes.s}>
+          <h1>{title}</h1>
+        </Box>
+      </motion.div>
+      <motion.div
+        variants={variants(delay + 0.2)}
+        animate="animate"
+        initial="initial"
+      >
+        <h3>{subtitle}</h3>
+      </motion.div>
     </Box>
   )
 }
