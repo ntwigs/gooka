184d9d9a4be4c916990ad477dd6b1e835f61e420
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-21 18:55:26 +0100

    fix: Remove unused logo styles

--- src/components/atoms/logo/index.tsx
+++ src/components/atoms/logo/index.tsx
@@ -1,9 +1,7 @@
 import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
-export const Logo = styled(motion.div)`
+export const Logo = styled(motion.img)`
   width: 60px;
   height: 60px;
-  background: ${({ theme }) => theme.colors.green};
-  border-radius: 30px;
 `
