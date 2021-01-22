aad664c8713a8b6b7d92cb338a3f90b80f963905
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-22 18:17:03 +0100

    feat: Animate changes

--- src/components/molecules/file-companion/index.tsx
+++ src/components/molecules/file-companion/index.tsx
@@ -1,59 +1,12 @@
 import * as React from 'react'
 import { motion } from 'framer-motion'
-import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { variants } from '../../../animations/animation'
-import {
-  getClassnames,
-  getFiles,
-  getStyles,
-} from '../../../redux/selectors/files'
-import { hasElements } from '../../../utils/has-elements'
 import { Box, Sizes } from '../../atoms/box'
 import files from '../../../assets/gooka_files.png'
-
-const useText = () => {
-  const classnames = useSelector(getClassnames)
-  const files = useSelector(getFiles)
-  const styles = useSelector(getStyles)
-
-  const hasClassnames = hasElements(classnames)
-  const hasFiles = hasElements(files)
-  const hasStyles = hasElements(styles)
-
-  if (hasClassnames) {
-    return {
-      title: 'Unused classnames',
-      subtitle: 'Found some classnames to remove.',
-      isEmpty: false,
-    }
-  }
-
-  if (hasFiles && hasStyles) {
-    return {
-      title: 'No unused classnames',
-      subtitle:
-        'There were no unused classnames in the files you provided to Gooka.',
-      isEmpty: true,
-    }
-  }
-
-  if (hasStyles) {
-    return {
-      title: 'Add files to compare',
-      subtitle:
-        'Drag and drop files from vscode to compare them and check for unused css classnames.',
-      isEmpty: false,
-    }
-  }
-
-  return {
-    title: 'Add stylesheet to compare',
-    subtitle:
-      'Drag and drop a style file from vscode to compare and check for unused css classnames.',
-    isEmpty: false,
-  }
-}
+import { useHasRendered } from '../../../utils/use-has-rendered'
+import { useWait } from '../../../utils/use-wait'
+import { useText } from './use-text'
 
 const Image = styled(motion.img)`
   width: 200px;
@@ -71,38 +24,54 @@ const H1 = styled(motion.h1)<H1Props>`
 const H3 = styled(motion.h3)`
   text-align: center;
   max-width: 367px;
+  align-self: center;
 `
 
 export const FileCompanion = () => {
+  const DELAY = 1000
+  const hasRendered = useHasRendered()
   const { title, subtitle, isEmpty } = useText()
+  const { value: isEmptyValue } = useWait<boolean>({
+    value: isEmpty,
+    delay: DELAY,
+  })
+  const { value: titleValue } = useWait<string>({ value: title, delay: DELAY })
+  const { value: subtitleValue } = useWait<string>({
+    value: subtitle,
+    delay: DELAY,
+  })
+  const updated = title === titleValue ? 'animate' : 'exit'
+  const variantDelay = hasRendered ? 0 : 3
+  const variantTitleDelay = hasRendered ? 0.2 : 3.2
+  const variantTextDelay = hasRendered ? 0.4 : 3.4
 
   return (
     <Box>
       <Box marginBottom={Sizes.l}>
         <Image
           src={files}
-          variants={variants(3)}
+          variants={variants(variantDelay)}
           animate="animate"
           initial="initial"
           exit="exit"
         />
       </Box>
       <H1
-        variants={variants(3.2)}
-        animate="animate"
+        variants={variants(variantTitleDelay)}
+        animate={updated}
         initial="initial"
         exit="exit"
-        isEmpty={isEmpty}
+        isEmpty={isEmptyValue}
       >
-        {title}
+        {titleValue}
       </H1>
       <H3
-        variants={variants(3.4)}
-        animate="animate"
+        variants={variants(variantTextDelay)}
+        animate={updated}
         initial="initial"
         exit="exit"
       >
-        {subtitle}
+        {subtitleValue}
       </H3>
     </Box>
   )
