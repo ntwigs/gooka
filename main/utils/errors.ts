c033a2086f83af251e8afaa60ff9e4829e5d8d5e
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-23 16:36:19 +0100

    feat: Create error types

---
+++ main/utils/errors.ts
@@ -0,0 +1,6 @@
+export class GetContentError extends Error {}
+export class GetPathError extends Error {}
+export class GetClassnameError extends Error {}
+export class GetCleanClassnameError extends Error {}
+export class getUniqueClassnamesError extends Error {}
+export class GetUnusedClassnamesError extends Error {}
