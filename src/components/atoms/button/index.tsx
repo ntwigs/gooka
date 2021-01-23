f929bbcbf78b71db8d4bd94b2f9df58443ddd4be
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-23 16:37:35 +0100

    feat: Animate buttons

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,6 +1,9 @@
-import styled, { DefaultTheme, useTheme } from 'styled-components'
+import { motion } from 'framer-motion'
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+import { variants } from '../../../animations/animation'
 
-type ButtonProps = {
+type StyledButtonProps = {
   apperance?: 'warning' | 'standard'
   disabled?: boolean
 }
@@ -13,7 +16,7 @@ const getBackground = ({ apperance, theme }: any) => {
   return theme.colors.red
 }
 
-export const Button = styled.button<ButtonProps>`
+export const StyledButton = styled.button<StyledButtonProps>`
   background: ${getBackground};
   color: ${(props) => props.theme.colors.white};
   border-radius: 10px;
@@ -25,5 +28,29 @@ export const Button = styled.button<ButtonProps>`
   outline: none;
   cursor: pointer;
   pointer-events: ${(props) => (props.disabled ? 'none' : 'all')};
-  opacity: ${(props) => (props.disabled ? 0.2 : 1)};
 `
+
+type ButtonProps = {
+  children: ReactNode
+  onClick: () => void
+} & StyledButtonProps
+
+export const Button = ({
+  children,
+  apperance,
+  disabled,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <motion.div
+      variants={variants(0)}
+      animate={disabled ? 'disabled' : 'animate'}
+      whileHover={disabled ? 'disabled' : 'hover'}
+      whileTap={disabled ? 'disabled' : 'click'}
+    >
+      <StyledButton apperance={apperance} disabled={disabled} onClick={onClick}>
+        {children}
+      </StyledButton>
+    </motion.div>
+  )
+}
