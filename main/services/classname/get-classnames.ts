2fb8a6291da6e93185a3e5c876bac6fc07faa9f1
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-23 16:34:54 +0100

    feat: Catch classsnames error

--- main/services/classname/get-classnames.ts
+++ main/services/classname/get-classnames.ts
@@ -1,4 +1,13 @@
+import { GetClassnameError } from '../../utils/errors'
+import { withError } from '../../utils/with-error'
+
 export const getClassnames = (content: string): string[] => {
   const css = /\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*\s*/g
-  return content.match(css) || []
+  const { error, result } = withError(() => content.match(css) || [])
+
+  if (error) {
+    throw new GetClassnameError('Could not get classnames.')
+  }
+
+  return result
 }
