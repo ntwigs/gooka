73e270fbad6a23dfccc1724011fbdb164bcab225
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-23 16:35:01 +0100

    feat: Clean classnames error

--- main/services/classname/get-clean-classnames.ts
+++ main/services/classname/get-clean-classnames.ts
@@ -1,2 +1,14 @@
-export const getCleanClassnames = (content: string[]): string[] =>
-  content.map((classname) => classname.trim())
+import { GetCleanClassnameError } from '../../utils/errors'
+import { withError } from '../../utils/with-error'
+
+export const getCleanClassnames = (content: string[]): string[] => {
+  const { error, result } = withError(() =>
+    content.map((classname) => classname.trim()),
+  )
+
+  if (error) {
+    throw new GetCleanClassnameError('Could not get clean classnames.')
+  }
+
+  return result
+}
