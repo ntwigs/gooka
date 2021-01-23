3a17b54f4a72b7c306e8703bba26d1132e6215e4
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-23 16:35:55 +0100

    feat: Handle unused classnames error

--- main/services/classname/get-unused-classnames.ts
+++ main/services/classname/get-unused-classnames.ts
@@ -1,7 +1,19 @@
+import { GetUnusedClassnamesError } from '../../utils/errors'
+import { withError } from '../../utils/with-error'
+
 type GetUnusedProps = {
   styleClasses: string[]
   fileClasses: string[]
 }
 
-export const getUnused = ({ styleClasses, fileClasses }: GetUnusedProps) =>
-  styleClasses.filter((x) => fileClasses.indexOf(x) < 0)
+export const getUnused = ({ styleClasses, fileClasses }: GetUnusedProps) => {
+  const { error, result } = withError(() =>
+    styleClasses.filter((x) => fileClasses.indexOf(x) < 0),
+  )
+
+  if (error) {
+    throw new GetUnusedClassnamesError('Could not get unused classnames.')
+  }
+
+  return result
+}
