904612b3aaf8088c8b75f31853dd881dacec9d9f
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-23 16:35:39 +0100

    feat: Handle unique classname error

--- main/services/classname/get-unique-classnames.ts
+++ main/services/classname/get-unique-classnames.ts
@@ -1,7 +1,16 @@
+import { getUniqueClassnamesError } from '../../utils/errors'
+import { withError } from '../../utils/with-error'
+
 type GetUniqueClassnames = {
   classnames: string[]
 }
 
-export const getUniqueClassnames = ({ classnames }: GetUniqueClassnames) => [
-  ...new Set(classnames),
-]
+export const getUniqueClassnames = ({ classnames }: GetUniqueClassnames) => {
+  const { error, result } = withError(() => [...new Set(classnames)])
+
+  if (error) {
+    throw new getUniqueClassnamesError('Could not get unique classnames.')
+  }
+
+  return result
+}
