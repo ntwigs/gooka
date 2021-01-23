6c5cc2536b3b2321672ec868e9fd340f917f36b1
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-23 16:38:41 +0100

    feat: Use animated buttons

--- src/components/organisms/action-section/index.tsx
+++ src/components/organisms/action-section/index.tsx
@@ -8,6 +8,8 @@ import { getFiles, getStyles } from '../../../redux/selectors/files'
 import { hasElements } from '../../../utils/has-elements'
 import { Box, Sizes } from '../../atoms/box'
 import { Button } from '../../atoms/button'
+import { useAlert } from '../../molecules/alert/context'
+import { useReCompare } from './use-recompare'
 
 const ActionSectionContainer = styled.div`
   display: flex;
@@ -16,6 +18,7 @@ const ActionSectionContainer = styled.div`
 `
 
 export const ActionSection = () => {
+  const recompare = useReCompare()
   const files = useSelector(getFiles)
   const styles = useSelector(getStyles)
   const dispatch = useDispatch()
@@ -24,21 +27,20 @@ export const ActionSection = () => {
   const hasFiles = hasElements(files)
   const hasStyles = hasElements(styles)
 
+  const isResetDisabled = !hasFiles && !hasStyles
+  const isRecompareDisabled = !hasFiles || !hasStyles
+
   return (
     <ActionSectionContainer>
       <motion.div variants={variants(2.8)} animate="animate" initial="initial">
         <Box marginRight={Sizes.m}>
-          <Button onClick={reset} disabled={!hasFiles && !hasStyles}>
+          <Button onClick={recompare} disabled={isRecompareDisabled}>
             Recompare
           </Button>
         </Box>
       </motion.div>
       <motion.div variants={variants(3)} animate="animate" initial="initial">
-        <Button
-          apperance="warning"
-          onClick={reset}
-          disabled={!hasFiles && !hasStyles}
-        >
+        <Button apperance="warning" onClick={reset} disabled={isResetDisabled}>
           Reset
         </Button>
       </motion.div>
