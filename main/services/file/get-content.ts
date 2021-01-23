e3290c9b241cacd053f931797e0cc4256717f0c9
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-23 16:36:06 +0100

    feat: Handle content error

--- main/services/file/get-content.ts
+++ main/services/file/get-content.ts
@@ -1,4 +1,15 @@
 import * as fs from 'fs'
+import { GetContentError } from '../../utils/errors'
+import { withError } from '../../utils/with-error'
 
-export const getContent = (path: string) =>
-  fs.readFileSync(path, { encoding: 'utf-8' })
+export const getContent = (path: string) => {
+  const { error, result } = withError<string>(() =>
+    fs.readFileSync(path, { encoding: 'utf-8' }),
+  )
+
+  if (error) {
+    throw new GetContentError('Could not get content from provided files.')
+  }
+
+  return result
+}
