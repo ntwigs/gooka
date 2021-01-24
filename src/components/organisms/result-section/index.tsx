8e3798e0e3407ec65b8a3eb5f3d68e8535525096
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-24 14:40:55 +0100

    fix: Move classnamepresentation

--- src/components/organisms/result-section/index.tsx
+++ src/components/organisms/result-section/index.tsx
@@ -1,17 +1,14 @@
 import * as React from 'react'
 import { useSelector } from 'react-redux'
 import { getClassnames } from '../../../redux/selectors/files'
-import { ClassnameProps } from '../../../redux/types/files'
 import { hasElements } from '../../../utils/has-elements'
-import { Box, Sizes } from '../../atoms/box'
-import { HeaderSpacer } from '../../atoms/header-spacer'
-import { UnnecessaryClassname } from '../../molecules/unnecessary-classname'
+import { Box } from '../../atoms/box'
 import { useCompare } from './use-compare'
 import { useClassnames } from './use-classnames'
-import { Header } from '../../molecules/header'
 import { FileCompanion } from '../../molecules/file-companion'
 import { Scroll } from '../../atoms/scroll'
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
+import { ClassnamePresentation } from '../../molecules/classname-presentation'
 
 export const ResultSection = () => {
   useClassnames()
@@ -19,29 +16,12 @@ export const ResultSection = () => {
   const classnames = useSelector(getClassnames)
   const hasClassnames = hasElements(classnames)
 
-  const Classnames = () => (
-    <AnimatePresence>
-      {classnames.map(({ name, id }: ClassnameProps, index: number) => (
-        <UnnecessaryClassname key={index} id={id} name={name} delay={index} />
-      ))}
-    </AnimatePresence>
-  )
-
   return (
     <Box grow>
       <Scroll>
         <AnimatePresence exitBeforeEnter>
           {!hasClassnames && <FileCompanion key="filecompanion" />}
-          {hasClassnames && (
-            <motion.div key="classes" exit={{ opacity: 0 }}>
-              <HeaderSpacer />
-              <Header
-                title="Unused classnames"
-                subtitle="Found some classnames to remove."
-              />
-              <Classnames />
-            </motion.div>
-          )}
+          {hasClassnames && <ClassnamePresentation classnames={classnames} />}
         </AnimatePresence>
       </Scroll>
     </Box>
