f76394542d14df85b314c364e13f9130bf5a0a1b
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-24 14:35:01 +0100

    feat: getIsLoading selector

--- src/redux/selectors/files.ts
+++ src/redux/selectors/files.ts
@@ -1,3 +1,4 @@
 export const getFiles = (state: any) => state.files.files
 export const getStyles = (state: any) => state.files.styles
 export const getClassnames = (state: any) => state.files.classnames
+export const getIsLoading = (state: any) => state.files.isLoading
