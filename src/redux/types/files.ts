b1757e48cea0543a42f6d78b2eac5b67fc3991d1
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-24 14:34:51 +0100

    feat: Create setIsLoading type

--- src/redux/types/files.ts
+++ src/redux/types/files.ts
@@ -4,6 +4,7 @@ export const SET_STYLES = 'SET_STYLE'
 export const REMOVE_STYLE = 'REMOVE_STYLE'
 export const RESET = 'RESET'
 export const SET_CLASSNAMES = 'SET_CLASSNAMES'
+export const SET_IS_LOADING = 'SET_IS_LOADING'
 
 export type ClassnameProps = {
   name: string
