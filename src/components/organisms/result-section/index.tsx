4fc3eaea282a84ae889b3b8cd0502b59ff0993dd
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-26 19:00:31 +0100

    feat: Check for unused classnames

--- src/components/organisms/result-section/index.tsx
+++ src/components/organisms/result-section/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
-import { useSelector } from 'react-redux'
-import { getClassnames } from '../../../redux/selectors/files'
-import { hasElements } from '../../../utils/has-elements'
+import { getHasUnusedClassnames } from '../../../redux/selectors/files'
 import { Box } from '../../atoms/box'
 import { useCompare } from './use-compare'
 import { useClassnames } from './use-classnames'
@@ -9,19 +7,19 @@ import { FileCompanion } from '../../molecules/file-companion'
 import { Scroll } from '../../atoms/scroll'
 import { AnimatePresence } from 'framer-motion'
 import { ClassnamePresentation } from '../../molecules/classname-presentation'
+import { useSelector } from 'react-redux'
 
 export const ResultSection = () => {
   useClassnames()
   useCompare()
-  const classnames = useSelector(getClassnames)
-  const hasClassnames = hasElements(classnames)
+  const hasUnusedClassnames = useSelector(getHasUnusedClassnames)
 
   return (
     <Box grow>
       <Scroll>
         <AnimatePresence exitBeforeEnter>
-          {!hasClassnames && <FileCompanion key="filecompanion" />}
-          {hasClassnames && <ClassnamePresentation classnames={classnames} />}
+          {!hasUnusedClassnames && <FileCompanion key="filecompanion" />}
+          {hasUnusedClassnames && <ClassnamePresentation key="presentation" />}
         </AnimatePresence>
       </Scroll>
     </Box>
