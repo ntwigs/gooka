3c2c3571135faf61dbc6378afbdf022a970580a5
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-26 19:01:49 +0100

    feat: HasUnusedClassname selector

--- src/redux/selectors/files.ts
+++ src/redux/selectors/files.ts
@@ -1,4 +1,5 @@
 export const getFiles = (state: any) => state.files.files
 export const getStyles = (state: any) => state.files.styles
 export const getClassnames = (state: any) => state.files.classnames
-export const getIsLoading = (state: any) => state.files.isLoading
+export const getHasUnusedClassnames = (state: any) =>
+  state.files.hasUnusedClassnames
