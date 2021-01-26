aafe74d5a34405ac995ef3ca12a7b37f70f0a1ed
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-26 19:01:35 +0100

    feat: Reset classname type

--- src/redux/types/files.ts
+++ src/redux/types/files.ts
@@ -4,7 +4,7 @@ export const SET_STYLES = 'SET_STYLE'
 export const REMOVE_STYLE = 'REMOVE_STYLE'
 export const RESET = 'RESET'
 export const SET_CLASSNAMES = 'SET_CLASSNAMES'
-export const SET_IS_LOADING = 'SET_IS_LOADING'
+export const RESET_CLASSNAMES = 'RESET_CLASSNAMES'
 
 export type ClassnameProps = {
   name: string
