bafe1c8c2c38f0ba6bc6c3100a6458838b3f7a83
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-31 11:39:28 +0100

    style: Overlay sizing

--- src/components/atoms/overlay/index.tsx
+++ src/components/atoms/overlay/index.tsx
@@ -5,8 +5,8 @@ export const Overlay = styled(motion.div)`
   position: absolute;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background: ${({ theme }) => theme.colors.white};
   z-index: 2;
   pointer-events: none;
