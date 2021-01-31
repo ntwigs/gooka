e504d2a40e3aa5d352fc868cb3af6bed4a15b227
ntwigs <oscar.cv.nordquist@gmail.com>
2021-01-31 11:37:08 +0100

    feat: Responsive layout

--- src/components/atoms/layout/index.tsx
+++ src/components/atoms/layout/index.tsx
@@ -3,14 +3,12 @@ import styled from 'styled-components'
 export const Layout = styled.div`
   overflow: hidden;
   height: 100vh;
-  width: 100vw;
   display: flex;
   flex: 1;
 
   @media (max-width: 800px) {
     flex-direction: column;
-    overflow-y: auto;
-    overflow-x: hidden;
     height: auto;
+    width: 100%;
   }
 `
