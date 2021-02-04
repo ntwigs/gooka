f700bcd7c1620c628274ccfb367d919dde772170
ntwigs <oscar.cv.nordquist@gmail.com>
2021-02-04 17:11:28 +0100

    feat: Use theme context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Start } from './components/pages/start'
-import { ThemeProvider } from 'styled-components'
-import { dark, light } from './theme'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import { AnimatedOverlay } from './components/atoms/overlay'
 import { AlertProvider } from './components/molecules/alert/context'
 import { GlobalStyle } from './theme/global-style'
+import { ThemeContextProvider } from './components/molecules/theme-button/context'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={dark}>
+      <ThemeContextProvider>
         <GlobalStyle />
         <AlertProvider>
           <AnimatedOverlay />
           <Start />
         </AlertProvider>
-      </ThemeProvider>
+      </ThemeContextProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
