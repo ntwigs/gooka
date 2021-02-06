ff2f613a4c5fcb8094219d9329e2f808cf63649c
ntwigs <oscar.cv.nordquist@gmail.com>
2021-02-06 15:44:26 +0100

    feat: Select IDs and classnames

--- main/services/classname/get-classnames.ts
+++ main/services/classname/get-classnames.ts
@@ -2,7 +2,7 @@ import { GetClassnameError } from '../../utils/errors'
 import { withError } from '../../utils/with-error'
 
 export const getClassnames = (content: string): string[] => {
-  const css = /\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*\s*/g
+  const css = /[.|#]-?[_a-zA-Z]+[_a-zA-Z0-9-]*\s*/g
   const { error, result } = withError(() => content.match(css) || [])
 
   if (error) {
