a6e6bd6847dcd2ef935e1636e55504cd206b08e0
ntwigs <oscar.cv.nordquist@gmail.com>
2021-02-06 15:45:15 +0100

    feat: Add errors for usage

--- main/utils/errors.ts
+++ main/utils/errors.ts
@@ -1,6 +1,8 @@
 export class GetContentError extends Error {}
 export class GetPathError extends Error {}
 export class GetClassnameError extends Error {}
+export class GetUsageError extends Error {}
 export class GetCleanClassnameError extends Error {}
 export class getUniqueClassnamesError extends Error {}
 export class GetUnusedClassnamesError extends Error {}
+export class GetSymbolRemovalError extends Error {}
