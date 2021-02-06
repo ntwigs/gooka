040f63618b5ec43b7c4bdc95df9326ceae896c07
ntwigs <oscar.cv.nordquist@gmail.com>
2021-02-06 15:45:09 +0100

    chore: Export getUsage service

--- main/services/classname/index.ts
+++ main/services/classname/index.ts
@@ -2,10 +2,12 @@ import { getClassnames } from './get-classnames'
 import { getCleanClassnames } from './get-clean-classnames'
 import { getUniqueClassnames } from './get-unique-classnames'
 import { getUnused } from './get-unused-classnames'
+import { getUsage } from './get-usage'
 
 export const classnames = {
   getUnused,
   getUniqueClassnames,
   getCleanClassnames,
   getClassnames,
+  getUsage,
 }
