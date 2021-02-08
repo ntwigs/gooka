395321652479cb1d4873ec60f376474f2497be71
ntwigs <oscar.cv.nordquist@gmail.com>
2021-02-08 18:56:10 +0100

    refactor: Remove unnecessary checks

--- main/preload.ts
+++ main/preload.ts
@@ -4,9 +4,8 @@ import { ipcRenderer, contextBridge, shell } from 'electron'
 // https://www.electronjs.org/docs/tutorial/context-isolation
 contextBridge.exposeInMainWorld('ipcRenderer', {
   send: ipcRenderer.send,
-  on: (channel: string, callback: (_: any, data: unknown) => void) => {
-    const newCallback = (_: unknown, data: unknown) => callback(_, data)
-    ipcRenderer.on(channel, newCallback)
+  on: (channel: string, callback: () => void) => {
+    ipcRenderer.on(channel, callback)
   },
 })
 
